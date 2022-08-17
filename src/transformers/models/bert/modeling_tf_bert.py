65918d939e9b28121c146afb3b352ca8d45df814
Kobster2434 <kobe@knowlesey.com>
2022-08-17 20:23:23 +1200

    temporary code for testing

--- src/transformers/models/bert/modeling_tf_bert.py
+++ src/transformers/models/bert/modeling_tf_bert.py
@@ -599,7 +599,7 @@ class TFBertEncoder(tf.keras.layers.Layer):
             attention_mask = attention_mask[:,:,:,self.config.num_aux_toks:]
             if self.config.is_diagnostics: print(f"attention_mask.shape: {attention_mask.shape}\n"
                                                  f"self.config.num_aux_toks: {self.config.num_aux_toks}")
-            print(f"aux_attn_mask: {aux_attn_mask}\nattention_mask[:, :, :, :self.config.num_aux_toks]): {attention_mask[:, :, :, :self.config.num_aux_toks])}")
+            print(f"aux_attn_mask: {aux_attn_mask}\nattention_mask[:, :, :, :self.config.num_aux_toks]): {attention_mask[:, :, :, :self.config.num_aux_toks]}")
             assert tf.reduce_all(tf.equal(aux_attn_mask, attention_mask[:, :, :, :self.config.num_aux_toks])), f"Expext this to raise an exception"
             #assert len(attention_mask.shape) == 2, f"len(attention_mask.shape): {len(attention_mask.shape)}, expected: {2}"
             #assert attention_mask.shape[1] == self.config.max_seq_len, f"attention_mask.shape: {attention_mask.shape}\n" \

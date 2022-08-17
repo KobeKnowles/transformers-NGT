652759f7cf67857b88f1f50ff957837414f72bda
Kobster2434 <kobe@knowlesey.com>
2022-08-17 20:50:56 +1200

    temporary code for testing

--- src/transformers/models/bert/modeling_tf_bert.py
+++ src/transformers/models/bert/modeling_tf_bert.py
@@ -577,36 +577,19 @@ class TFBertEncoder(tf.keras.layers.Layer):
             aux_tok_positions = hidden_states[:,:self.config.num_aux_toks,:]
             if self.config.is_diagnostics: print(f"aux_tok_positions.shape: {aux_tok_positions.shape}\n"
                                                  f"self.config.num_aux_toks: {self.config.num_aux_toks}")
-            #assert len(aux_tok_positions.shape) == 3, f"len(aux_tok_positions.shape): {len(aux_tok_positions.shape)}, expected: {3}"
-            #assert aux_tok_positions.shape[1] == self.config.num_aux_toks, f"aux_tok_positions.shape: {aux_tok_positions.shape}\n" \
-            #                                                               f"max_seq_len: {self.config.max_seq_len}"
+
             hidden_states = hidden_states[:,self.config.num_aux_toks:,:]
             if self.config.is_diagnostics: print(f"hidden_states.shape: {hidden_states.shape}\n"
                                                  f"self.config.num_aux_toks: {self.config.num_aux_toks}")
-            #assert len(hidden_states.shape) == 3, f"len(hidden_states.shape): {len(hidden_states.shape)}, expected: {3}"
-            #assert hidden_states.shape[1] == self.config.max_seq_len, f"hidden_states.shape: {hidden_states.shape}\n" \
-            #                                                          f"max_seq_len: {self.config.max_seq_len}"
 
             aux_attn_mask = attention_mask[:,:,:,:self.config.num_aux_toks]
             if self.config.is_diagnostics: print(f"aux_attn_mask.shape: {aux_attn_mask.shape}\n"
                                                  f"self.config.num_aux_toks: {self.config.num_aux_toks}")
-            assert tf.reduce_all(tf.equal(aux_attn_mask, attention_mask[:, :, :, :self.config.num_aux_toks]))
 
-            #assert len(aux_attn_mask.shape) == 2, f"len(aux_attn_mask.shape): {len(aux_attn_mask.shape)}, expected: {2}"
-            #assert aux_attn_mask.shape[1] == self.config.num_aux_toks, f"aux_attn_mask.shape: {aux_attn_mask.shape}\n" \
-            #                                                          f"max_seq_len: {self.config.max_seq_len}"
-            print(f"attention_mask[:, :, :, :self.config.num_aux_toks]b4: {attention_mask[:, :, :, :self.config.num_aux_toks]}")
             attention_mask = attention_mask[:,:,:,self.config.num_aux_toks:]
             if self.config.is_diagnostics: print(f"attention_mask.shape: {attention_mask.shape}\n"
                                                  f"self.config.num_aux_toks: {self.config.num_aux_toks}")
-            print(f"aux_attn_mask: {aux_attn_mask}\nattention_mask[:, :, :, :self.config.num_aux_toks]): {attention_mask[:, :, :, :self.config.num_aux_toks]}")
-            assert tf.reduce_all(tf.equal(aux_attn_mask, attention_mask[:, :, :, :self.config.num_aux_toks])), f"Expext this to raise an exception"
-            #assert len(attention_mask.shape) == 2, f"len(attention_mask.shape): {len(attention_mask.shape)}, expected: {2}"
-            #assert attention_mask.shape[1] == self.config.max_seq_len, f"attention_mask.shape: {attention_mask.shape}\n" \
-            #                                                           f"max_seq_len: {self.config.max_seq_len}"
 
-
-            #assert tf.reduce_all(tf.equal(x2, x[:, :2, :])).numpy().tolist()
 
         assert len(hidden_states.shape) == 3 and len(aux_tok_positions.shape) == 3 and len(aux_attn_mask.shape) == 4 \
                and len(attention_mask.shape) == 4

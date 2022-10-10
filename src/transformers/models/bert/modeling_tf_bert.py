23abd29c04a01bcf6641856bed11b87686a279e2
Kobster2434 <kobe@knowlesey.com>
2022-10-10 13:28:15 +1300

    added assert to test input_ids shape

--- src/transformers/models/bert/modeling_tf_bert.py
+++ src/transformers/models/bert/modeling_tf_bert.py
@@ -563,9 +563,10 @@ class TFBertEncoder(tf.keras.layers.Layer):
     def remove_pad_tok_positions(self, hidden_states, input_ids, pad_tok_id):
         #hidden_states.shape == (seq_len, hdim)
         #input_ids = (seq_len)
+        assert (input_ids.shape == 1)
         pos = None
         for i in range(len(input_ids)):
-            print(input_ids)
+            #print(input_ids)
             if input_ids[i] == pad_tok_id:
                 pos = i
                 break
